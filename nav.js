49102123362c5df9ecec52ba59e3e04f222309ba
edwina-droid <ednaomorodion@gmail.com>
2022-08-29 23:46:25 +0200

    Deleting irrelevant image and js file

--- nav.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $(".navbar-brand").click(function(){
-        $(".navbar-toggler").addClass("collapsed");
-        $(".navbar-toggler").attr("aria-expanded","false");
-        $(".navbar-collapse").removeClass("show");
-    });
-    
-    });
